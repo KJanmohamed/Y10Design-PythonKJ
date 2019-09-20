6fd0287e8c17c6fd66af87199cf087547b285240
Kalen Janmohamed <kalen.janmohamed@BoardManGetsPaid.local>
2019-09-20 12:30:00 -0400

    hi

---
+++ VendingMachine.py
@@ -0,0 +1,21 @@
+#This program simulates a vending machine
+#Authors: Kalen Janmohamed
+#Date: September 20- 2019
+#Upper Canada College
+
+print (" \n ")
+print ("Hello! Welcome to the Worlds cheapest vending machine! \n ")
+print ("Please make your selection \n ")
+
+print(" 				ROW 1 - DRINK ")
+
+
+print (" 1. Coke 	    2. Sprite 	    3. Crush 	    4. Peace Tea - Mango")
+print (" $1.50 		    $1.50	    $2.00	    $2.25	\n ")
+
+print(" 				ROW 2 - FOOD ")
+
+
+print (" 1.  	    2. Sprite 	    3. Crush 	    4. Peace Tea - Mango")
+print (" $1.50 		    $1.50	    $2.00	    $2.25	\n ")
+
